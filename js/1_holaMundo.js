be3d156262ffc30507b33c9c0685729be078a1b5
ShDigui <diegosalazae2003@gmail.com>
2022-02-07 16:25:23 -0500

    costos de producto en javascript

--- js/1_holaMundo.js
+++ js/1_holaMundo.js
@@ -2,4 +2,27 @@
 console.log("Hola mundo!!")
 //ejercicio 2//
 let nombre = "Diego"
-console. log ( "bienvenido(a), Sr(a) " + nombre) 
+console. log ( "bienvenido(a), Sr(a) " + nombre)
+
+/*declarar una variable llamada costoProducto con su respectivo valor y agregar 10% por concepto de impuestos y $2000 por domicilio 
+utilizando variables que permitan la mantebilidad y escalabilidad. Al final del ejercicio mostrar el Valor inicial, Subtotal (valor incial+impuesto)
+y valor total*/
+
+let costoProducto = 20000;
+let valorDomicilio =2000;
+let conceptoImpuesto = 0.10;
+
+
+let precioInicial = costoProducto ;
+console.log ("El precio incial del producto es: "+precioInicial);
+
+let subPrecio = precioInicial+(precioInicial*conceptoImpuesto);
+console.log ("El subprecio del producto es: "+subPrecio)
+
+let precioTotal =  subPrecio+valorDomicilio
+console.log ("El precio total del producto incluido el impuesto y el valor del domicilio es: "+precioTotal);
+
+
+
+
+
