3d7c3769e08b9c86708b995335a5ef87022a28ff
ShDigui <diegosalazae2003@gmail.com>
2022-02-07 17:09:00 -0500

    ejercicio javascript

--- js/1_holaMundo.js
+++ js/1_holaMundo.js
@@ -23,6 +23,28 @@ let precioTotal =  subPrecio+valorDomicilio
 console.log ("El precio total del producto incluido el impuesto y el valor del domicilio es: "+precioTotal);
 
 
+/*Declarar una variable llamada costoInicial. Si el producto es menor  a $30000 no tendra nngun tipo de desuento
+si el producto es igual o superior a $30000e inferior a $100000 tendra un descuento del 5%
+si el producto es igual o superior a $100000 el descuento sera del 10%
+imprimir el valor incial y el valor final (incluido el descuento).  */
+
+let costoInicial= 50000;
+let primerDescuento = 0.05;
+let segundoDescuento = 0.10;
+
+if (costoInicial <30000){ 
+    console.log ("El producto no aplica para ningun descuento")
+};
+
+if (costoInicial >=30000 && costoInicial <100000){
+    var valorDesc1 = costoInicial + (costoInicial-primerDescuento)
+    console.log ("El precio del poducto con un descuendo del 5% es: "+valorDesc1)
+};
+
+ if (costoInicial >=100000){
+     var valorDesc2 = costoInicial + (costoInicial-segundoDescuento)
+     console.log("El precio del producto con un descuento del 10% es: " + valorDesc2)
+ };
 
 
 
